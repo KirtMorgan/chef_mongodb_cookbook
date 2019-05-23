1fced11b7bcbd9285ab667cd21ff103dae9feed5
Kirt Morgan <kirtmorgan@live.com>
2019-05-23 11:51:25 +0100

    Added a working mongo cookbook

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook:: mongodb
+# Spec:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'mongodb::default' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+
+    it 'update all sources' do
+      expect(chef_run).to update_apt_update('update')
+    end
+
+
+    it 'should add mongo to the sources list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
+    end
+
+    it 'should install mongod' do
+      expect(chef_run).to upgrade_package('mongodb-org')
+    end
+
+  end
+
+end
